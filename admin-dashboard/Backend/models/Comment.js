8b9bedb3f7ef20e816480cb0a69e078dab4253d1
Devinda Jayathilaka <168094003+DevindaJ517@users.noreply.github.com>
2025-10-06 06:25:34 +0000

    update

--- admin-dashboard/Backend/models/Comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const commentSchema = new mongoose.Schema({
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  review: { type: mongoose.Schema.Types.ObjectId, ref: 'Review', required: true },
-  content: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Comment', commentSchema);
--- admin-dashboard/Backend/models/Setting.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const settingSchema = new mongoose.Schema({
-  key: { type: String, unique: true },
-  value: mongoose.Schema.Types.Mixed
-});
-
-module.exports = mongoose.model('Setting', settingSchema);
